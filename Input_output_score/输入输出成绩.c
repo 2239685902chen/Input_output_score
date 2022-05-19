c6b1b8c521ae7e417f4796e86c0de560d33766fc
chen <you@example.com>
2022-05-19 16:26:57 +0800

    添加项目文件。

---
+++ Input_output_score/输入输出成绩.c
@@ -0,0 +1,9 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int main()
+{
+	int q = 0, w = 0, e = 0;
+	scanf("%d%d%d", &q, &w, &e);
+	printf("score1=%d,score2=%d,score3=%d\n", q, w, e);
+	return 0;
+}
